5635c07358a5dc76225daec3278419f85376e4ca
joseramirez <joseramirezgomez00@gmail.com>
2026-01-29 22:50:51 -0500

    finish1

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,15 @@
-import axios from 'axios';
-const BASE = process.env.REACT_APP_API_BASE || 'http://localhost:4000/api';
-const api = axios.create({ baseURL: BASE, timeout: 10000 });
+import axios from "axios";
+
+const BASE =
+  process.env.REACT_APP_API_BASE?.replace(/\/$/, "") ||
+  "https://inventarioshalito.onrender.com/api";
+
+const api = axios.create({
+  baseURL: BASE,
+  timeout: 15000, // Render puede tardar si despierta
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
 export default api;
